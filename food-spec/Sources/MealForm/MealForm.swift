cc66bd3b1d337ae01eb8ea5d3eae71b15181a774
Victor Socaciu <socaciu.victor@gmail.com>
2023-12-15 10:07:01 +0400

    more work on form

--- food-spec/Sources/MealForm/MealForm.swift
+++ food-spec/Sources/MealForm/MealForm.swift
@@ -5,6 +5,7 @@ import ComposableArchitecture
 
 public struct MealForm: View {
     @Bindable var store: StoreOf<MealFormFeature>
+    @FocusState var focusedField: String?
 
     public init(store: StoreOf<MealFormFeature>) {
         self.store = store
@@ -14,16 +15,21 @@ public struct MealForm: View {
         Form {
             Section("Name") {
                 TextField("Name", text: $store.meal.sending(\.updateMeal).name)
+                    .submitLabel(.done)
+                    .focused($focusedField, equals: "name")
             }
 
             Section("Ingredients") {
-                List(store.meal.ingredients, id: \.food) { ingredient in
+                ForEach(store.meal.ingredients, id: \.food) { ingredient in
                     VStack(alignment: .leading) {
                         Text(ingredient.food.name)
                         Text(ingredient.quantity.formatted(width: .wide))
                             .font(.footnote)
                             .foregroundStyle(.secondary)
                     }
+                }
+                .onDelete { offsets in
+                    self.store.send(.onDeleteIngredients(offsets))
                 }
             }
 
@@ -37,20 +43,29 @@ public struct MealForm: View {
                 TextEditor(
                     text: $store.meal.sending(\.updateMeal).instructions
                 )
+                .focused($focusedField, equals: "instructions")
+                .frame(minHeight: 100)
             }
         }
         .formStyle(.grouped)
+        .scrollDismissesKeyboard(.immediately)
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 Button("Cancel") {
-
+                    self.store.send(.cancelButtonTapped)
                 }
             }
             ToolbarItem(placement: .topBarTrailing) {
                 Button("Save") {
-
+                    self.store.send(.saveButtonTapped)
                 }
-                .disabled(true)
+                .disabled(self.store.isSaveButtonDisabled)
+            }
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+                Button("Done") {
+                    self.focusedField = nil
+                }
             }
         }
         .navigationTitle("New Meal")
@@ -58,12 +73,34 @@ public struct MealForm: View {
 }
 
 #Preview {
-    MealForm(
-        store: .init(
-            initialState: MealFormFeature.State(),
-            reducer: {
-                MealFormFeature()
-            }
+    NavigationStack {
+        MealForm(
+            store: .init(
+                initialState: MealFormFeature.State(
+                    meal: .init(
+                        name: "name",
+                        ingredients: [
+                            .init(
+                                food: .preview(id: 1),
+                                quantity: .grams(100)
+                            ),
+                            .init(
+                                food: .preview(id: 2),
+                                quantity: .grams(150)
+                            ),
+                            .init(
+                                food: .preview(id: 3),
+                                quantity: .grams(230)
+                            ),
+                        ],
+                        servingSize: .grams(250),
+                        instructions: "instructions"
+                    )
+                ),
+                reducer: {
+                    MealFormFeature()
+                }
+            )
         )
-    )
+    }
 }
--- food-spec/Sources/MealForm/MealFormFeature.swift
+++ food-spec/Sources/MealForm/MealFormFeature.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Shared
+import Database
 import QuantityPicker
 import ComposableArchitecture
 
@@ -8,6 +9,7 @@ public struct MealFormFeature {
     @ObservableState
     public struct State: Hashable {
         var meal: Meal
+        var isSaveButtonDisabled: Bool
 
         var quantity: QuantityPickerFeature.State {
             get {
@@ -20,20 +22,28 @@ public struct MealFormFeature {
 
         public init() {
             self.meal = .empty
+            self.isSaveButtonDisabled = true
         }
 
         public init(meal: Meal) {
             self.meal = meal
+            self.isSaveButtonDisabled = false
         }
     }
 
     @CasePathable
     public enum Action {
+        case cancelButtonTapped
+        case saveButtonTapped
         case updateMeal(Meal)
         case quantityPicker(QuantityPickerFeature.Action)
+        case onDeleteIngredients(IndexSet)
     }
 
     public init() { }
+
+    @Dependency(\.dismiss) private var dismiss
+    @Dependency(\.databaseClient) private var databaseClient
 
     public var body: some ReducerOf<Self> {
         Scope(state: \.quantity, action: \.quantityPicker) {
@@ -41,13 +51,36 @@ public struct MealFormFeature {
         }
         Reduce { state, action in
             switch action {
+                case .cancelButtonTapped:
+                    return .run { [dismiss] _ in
+                        await dismiss()
+                    }
+                case .saveButtonTapped:
+                    // store in db then dismiss the view
+                    return .run { [databaseClient, dismiss, meal = state.meal] _ in
+                        try await databaseClient.insert(meal: meal)
+                        await dismiss()
+                    }
                 case .updateMeal(let meal):
                     state.meal = meal
+                    if isValid(meal) {
+                        state.isSaveButtonDisabled = false
+                    } else {
+                        state.isSaveButtonDisabled = true
+                    }
                     return .none
                 case .quantityPicker:
                     return .none
+                case .onDeleteIngredients(let indices):
+                    state.meal.ingredients.remove(atOffsets: indices)
+                    return .none
             }
         }
+    }
+
+    private func isValid(_ meal: Meal) -> Bool {
+        !meal.name.isEmpty &&
+        !meal.ingredients.isEmpty
     }
 }
 
